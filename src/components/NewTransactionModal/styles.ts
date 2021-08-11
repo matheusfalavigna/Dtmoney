bb1d2ea030620e0c76da5fab87b02abee9a4182f
Matheus Falavigna <60001410+matheusfalavigna@users.noreply.github.com>
2021-08-11 10:36:39 -0300

    Update styles.ts

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -4,7 +4,7 @@ import { darken, transparentize } from 'polished';
 export const Container = styled.form`
   h2 {
     color: var(--text-title);
-    font-size: 1.5em;
+    font-size: 1.5rem;
     margin-bottom: 2rem;
   }
 
